889d690cbdb27a658df90bf554360b3bb6040431
Sergii Oliinyk <oliinyk.sergii@gmail.net>
2014-12-03 13:04:30 +0200

    Tests refactoring

--- src/test/java/rest_tests/NegativeFlow.java
+++ src/test/java/rest_tests/NegativeFlow.java
@@ -1,6 +1,8 @@
 package rest_tests;
 
 import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.filter.LoggingFilter;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import javax.ws.rs.client.Client;
@@ -9,21 +11,36 @@ import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.logging.Logger;
 
 import static org.testng.Assert.assertEquals;
 
 public class NegativeFlow {
 
-    public static final String TEST_URL = "http://localhost:8080";
+    public static final String TEST_URL = "http://localhost:8080/rest";
+
+    public static final Logger LOGGER = Logger.getLogger(PositiveFlow.class.getName());
+
+    ClientConfig clientConfig;
+
+    Client client;
+
+    WebTarget target;
+
+    @BeforeTest
+    public void setUp() {
+
+        clientConfig = new ClientConfig();
+
+        clientConfig.register(new LoggingFilter(LOGGER, true));
+
+        client = ClientBuilder.newClient(clientConfig);
+
+        target = client.target(TEST_URL);
+    }
 
     @Test
     public void testCacheMaxAge() {
-
-        ClientConfig clientConfig = new ClientConfig();
-
-        Client client = ClientBuilder.newClient(clientConfig);
-
-        WebTarget target = client.target(TEST_URL);
 
         WebTarget cacheControl = target.path("blabla/blabla" + 10);
 
@@ -42,12 +59,6 @@ public class NegativeFlow {
 
     @Test
     public void testHTTPMethods() {
-
-        ClientConfig clientConfig = new ClientConfig();
-
-        Client client = ClientBuilder.newClient(clientConfig);
-
-        WebTarget target = client.target(TEST_URL);
 
         WebTarget cacheControl = target.path("blabla/blabla" + 10);
 
